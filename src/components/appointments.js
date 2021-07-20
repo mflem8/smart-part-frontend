30d2aca0243df442c64ee93ada279abf7e96f965
mflem8 <matthewfleming8@gmail.com>
2021-07-20 12:29:09 -0400

    built out component classes

---
+++ src/components/appointments.js
@@ -0,0 +1,15 @@
+class Appointments {
+    constructor() {
+        this.appointments = []
+        this.adapter = new AppointmentsAdapter()
+        this.bindEventListeners()
+        this.fetchAndLoadNotes()
+    }
+
+
+    fetchAndLoadNotes() {
+        this.adapter.getAppointments().then(appointments => {
+            console.log(appointments)
+        })
+    }
+}
